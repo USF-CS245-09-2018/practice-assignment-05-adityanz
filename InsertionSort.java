ffa74c47c12b5ca714ce797c3e99cdf6f88dfb88
adityanz <aditya@adityadixit.com>
2018-10-05 23:33:30 -0700

    Final

---
+++ InsertionSort.java
@@ -0,0 +1,17 @@
+public class InsertionSort implements SortingAlgorithm {
+    @Override
+    public void sort (int [] arr)
+    {
+        for (int i = 1; i < arr.length; i++) {
+            int temp = arr[i];
+            int k = i-1;
+            while (k >= 0 && arr[k] > temp)
+            {
+                arr[k+1] = arr [k];
+                --k;
+            }
+            arr[k+1] = temp;
+        }
+
+    }
+}
---
+++ out/production/practice-assignment-05-adityanz/README.md
@@ -0,0 +1,3 @@
+# CS 245 (Fall, 2018) Practice Assignment 05
+
+See assignment details on Canvas.
